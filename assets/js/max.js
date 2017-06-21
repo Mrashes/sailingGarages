5438e4dab7a2570a7448e7b16a7673a535986293
Max Rashes <mrashes2@gmail.com>
2017-06-20 21:16:37 -0500

    finish functionality

--- assets/js/max.js
+++ assets/js/max.js
@@ -1,9 +1,31 @@
 var max = {
+	object: {},
 	popup: function() {
-		    $('#popup').html('<div style="position:absolute; width:800px; margin:5%; background-color: green;">centered content</div>')
+		   $('#popup').html('<div style="position:absolute; width:800px; margin:5%; background-color: green; display: flex-inline"><p>Title</p><input type="text" name="title" id="title"><p>Description</p><input type="text" name="description" id="description"><p>Dates</p><input type="text" name="dates" id="dates"><p>Start Time</p><input type="text" name="start" id="start"><p>End Time</p><input type="text" name="end" id="end"><p>Location</p><input type="text" name="location" id="location"><p>Keywords</p><input type="text" name="keyword" id="keyword"><button id="submit">submit</button></div>')
+	},
+	submit: function() {
+		max.object.title = $('#title').val();
+		$('#title').val('');
+		max.object.description = $('#description').val();
+		$('#description').val('');
+		max.object.dates = $('#dates').val();
+		$('#dates').val('');
+		max.object.start = $('#start').val();
+		$('#start').val('');
+		max.object.end = $('#end').val();
+		$('#end').val('');
+		max.object.location = $('#location').val();
+		$('#location').val('');
+		max.object.keyword = $('#keyword').val();
+		$('#keyword').val('');
+		$('#popup').html("")
 	}
 }
 
 $(document).on('click', '#button', function() {
 	max.popup()
+});
+
+$(document).on('click', '#submit', function() {
+	max.submit()
 });
