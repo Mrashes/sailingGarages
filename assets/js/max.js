f8b74ca6ec0851abf907d1777094b7688af2cc7c
Max Rashes <mrashes2@gmail.com>
2017-06-22 20:07:06 -0500

    working on input box formatting

--- assets/js/max.js
+++ assets/js/max.js
@@ -1,27 +1,30 @@
 var max = {
 	object: {},
 	popup: function() {
-		$('#popup').html('<div style="position:absolute; width:800px; margin:5%; background-color: green; display: flex-inline"><p>Title</p><input type="text" name="title" id="title"><p>Description</p><input type="text" name="description" id="description"><p>Dates</p><input type="date" name="date" id="date"><p>Start Time</p><input type="time" name="start" id="start"><p>End Time</p><input type="time" name="end" id="end"><p>Location</p><input type="text" name="location" id="location"><p>Keywords</p><input type="text" name="keyword" id="keyword"><button id="submit">submit</button></div>')
+		$('#popup').html('<div class="popup"><p>Title</p><input type="text" name="title" id="title"><p>Description</p><input type="text" name="description" id="description"><p>Dates</p><input type="date" name="date" id="date"><p>Start Time</p><input type="time" name="start" id="start"><p>End Time</p><input type="time" name="end" id="end"><p>Location</p><input type="text" name="location" id="location"><p>Keywords</p><input type="text" name="keyword" id="keyword"><button id="submit">submit</button></div>')
 	},
 	submit: function() {
 		max.object.title = $('#title').val();
 		max.object.description = $('#description').val();
-		max.object.dates = $('#dates').val();
+		max.object.date = $('#date').val();
 		max.object.start = $('#start').val();
 		max.object.end = $('#end').val();
 		max.object.location = $('#location').val();
 		max.object.keyword = $('#keyword').val();
 	},
-	clearInput: function() {
+	clearInputs: function() {
 		$('#keyword').val('');
 		$('#location').val('');
 		$('#end').val('');
 		$('#start').val('');
-		$('#dates').val('');
+		$('#date').val('');
 		$('#description').val('');
 		$('#title').val('');
-		$('#popup').html('');
+		
 	},
+	clearPopup: function(){
+		$('#popup').html('');
+	}
 }
 
 $(document).on('click', '#button', function() {
@@ -29,5 +32,8 @@ $(document).on('click', '#button', function() {
 });
 
 $(document).on('click', '#submit', function() {
-	app.addNewListing()
+	// max.submit();
+	app.addNewListing();
+	max.clearPopup()
+	setTimeout(clearInputs, 1000)
 });
