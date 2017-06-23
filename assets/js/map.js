f2f4ec78e4c4e81b2aebe03449d4446313539f40
Jeremy Lipsitz <jeremylipsitz@gmail.com>
2017-06-22 20:11:07 -0500

    added map.js

---
+++ assets/js/map.js
@@ -0,0 +1,12 @@
+function initMap() {
+        var chicago = {lat: 41.878, lng: -87.630};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: chicago
+        });
+        var marker = new google.maps.Marker({
+          position: chicago,
+          map: map
+        });
+      };
+      
